1461a7e0afa2a98b7a3a885e7e7a699966467194
Aliaksandr <alex.kamai@ya.ru>
2022-07-16 00:11:07 +0300

    Добавьте файлы проекта.

---
+++ Construction_Materials/Type.h
@@ -0,0 +1,33 @@
+#pragma once
+class Type
+{
+private:
+	string dimensions;
+	string grade;
+
+public:
+	Type() : dimensions("N/A"), grade("N/A")
+	{
+
+	}
+
+	Type(string di, string gr) : dimensions(di), grade(gr)
+	{
+
+	}
+
+	void gettype()
+	{
+		cout << "Enter nominal dimensions (2 x 4 and so on): ";
+		cin >> dimensions;
+		cout << "Enter grade (raw, timber and so on): ";
+		cin >> grade;
+	}
+
+	void showtype()
+	{
+		cout << "\nDimensions: " << dimensions;
+		cout << "\nVariety: " << grade;
+	}
+};
+
